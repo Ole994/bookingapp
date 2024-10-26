0da7cafadb2da3b24e40dd64f094d6a256f21c8f
Ole994 <69842859+Ole994@users.noreply.github.com>
2024-10-26 17:15:58 +0200

    Set base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
+  base: '/bookingapp/', // Angi basestien til repositorienavnet
   plugins: [react()],
   build: {
     target: 'esnext',
