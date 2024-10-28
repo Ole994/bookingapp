e00910f53740ea2ff2e5053a63400137324bdf1d
Ole994 <69842859+Ole994@users.noreply.github.com>
2024-10-28 18:07:14 +0100

    livepage

--- vite.config.js
+++ vite.config.js
@@ -4,60 +4,62 @@ import react from '@vitejs/plugin-react-swc';
 import { visualizer } from 'rollup-plugin-visualizer';
 import { VitePWA } from 'vite-plugin-pwa';
 
-// Bruker import.meta.env.MODE for å bestemme miljøet i Vite
-const base = import.meta.env.MODE === 'production' ? '/bookingapp/' : '/';
+// Setter base-URL basert på 'mode'-argumentet
+export default defineConfig(({ mode }) => {
+  const base = mode === 'production' ? '/bookingapp/' : '/';
 
-export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({ open: true }), // Åpner en grafisk rapport automatisk etter bygging
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Booking App',
-        short_name: 'Booking',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+  return {
+    plugins: [
+      react(),
+      visualizer({ open: true }), // Åpner en grafisk rapport automatisk etter bygging
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+        manifest: {
+          name: 'Booking App',
+          short_name: 'Booking',
+          theme_color: '#ffffff',
+          icons: [
+            {
+              src: 'icon-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+          ],
+        },
+      }),
+    ],
+    base, // Dynamisk base-URL
+    build: {
+      outDir: 'dist',
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            }
           },
-          {
-            src: 'icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-  base, // Dynamisk base-URL
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
         },
       },
     },
-  },
-  server: {
-    hmr: {
-      overlay: false,
+    server: {
+      hmr: {
+        overlay: false,
+      },
+      historyApiFallback: true,
     },
-    historyApiFallback: true,
-  },
-  optimizeDeps: {
-    include: [
-      'firebase/app',
-      'firebase/auth',
-      'firebase/firestore',
-      'firebase/analytics'
-    ]
-  }
+    optimizeDeps: {
+      include: [
+        'firebase/app',
+        'firebase/auth',
+        'firebase/firestore',
+        'firebase/analytics'
+      ]
+    }
+  };
 });
