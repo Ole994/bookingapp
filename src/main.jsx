23c18f67e3765a11bbfb58e0902e96b17e5afcf5
Ole994 <69842859+Ole994@users.noreply.github.com>
2024-10-28 18:25:18 +0100

    trying again to get the page to work error free live

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext'; // Importer AuthProvider
 import { initWebSocket } from './webSocketHandler'; // Importer WebSocket-håndtering
 import { ErrorProvider } from './component/error/ErrorContext'; 
@@ -14,13 +14,13 @@ initWebSocket();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider> {/* Pakk App med AuthProvider for å dele autentisering */}
       <ErrorProvider> 
           <App />
         </ErrorProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
